1a23af43c0c1cbac229ccb69eb25ce9444381979
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-04-30 06:31:39 +0100

    code confi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Layout } from "../components/layout"
-import { Home } from "../components/home"
-import { Movies } from "../routing/movie.jsx"
-import {Routes, Route} from "react-router-dom"
+import { Layout } from "../components/layout";
+import { Home } from "../components/home";
+import { Movies } from "../routing/movie.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 export const App = () => {
-  return(
+  return (
     <>
       <div>
         <Layout />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
           <Route path="/movies" element={<Movies />} />
         </Routes>
       </div>
     </>
-  )
-}
+  );
+};
